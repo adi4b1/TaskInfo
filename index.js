f8ea4c90e083560c7fc5c7061b416d2e51200e83
krishnachaitanyamitnala <120622684+krishnachaitanyamitnala@users.noreply.github.com>
2025-03-03 00:33:56 +0530

    c2index

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ const cors = require("cors");
 dotEnv.config();
 
 const app = express();
-app.use((req, res, next) => {
-  // res.header(
-  //   "Access-Control-Allow-Origin",
-  //   "https://task-backend-zeta-puce.vercel.app/"
-  // );
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // ✅ Allow frontend
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+
 app.options("*", cors()); // ✅ Handle preflight requests
 
 app.use(bodyParser.json());
