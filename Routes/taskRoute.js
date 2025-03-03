a76ea0f262439303b03db75a804d9ede475a7fb8
krishnachaitanyamitnala <120622684+krishnachaitanyamitnala@users.noreply.github.com>
2025-03-03 10:49:40 +0530

    taskRoute

--- Routes/taskRoute.js
+++ Routes/taskRoute.js
@@ -3,9 +3,6 @@ const taskRoute=require('../controllers/taskController')
 
 const router=express.Router()
 
-
-
-
 router.post('/add-task',taskRoute.addTask)
 router.get('/alltasks',taskRoute.allTasks)
 module.exports=router
