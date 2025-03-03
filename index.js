a0028d7f9d9385bf3c389aa30f70123d24ca8f51
krishnachaitanyamitnala <120622684+krishnachaitanyamitnala@users.noreply.github.com>
2025-03-03 11:07:11 +0530

    cv

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 const bodyParser = require("body-parser");
 const taskRoute = require("./Routes/taskRoute");
-const cors=require('cors')
+const cors = require("cors");
 dotEnv.config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 4000;
 //middleware to connect routes
 
 app.use("/task", taskRoute);
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Welcome to task monitor");
 });
 app.listen(PORT, (req, res) => {
