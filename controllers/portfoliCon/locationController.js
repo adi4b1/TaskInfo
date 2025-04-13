2dcdd2fa19845161465c37874caa0307ba4465f3
krishnachaitanyamitnala <120622684+krishnachaitanyamitnala@users.noreply.github.com>
2025-04-13 12:33:58 +0530

    kjhxdrjg

--- controllers/portfoliCon/locationController.js
+++ controllers/portfoliCon/locationController.js
@@ -5,10 +5,9 @@ const AddLocation=async(req,res)=>{
     const{location}=req.body
     console.log("Received location in backend:", location);
     try {
-        const saved = new Location({ location });
-        await saved.save()
-
-        return res.status(201).json({success:"location created"})
+        const saved = await Location.create({ location });
+    console.log("✅ Location saved:", saved);
+    return res.status(201).json({ success: "Location created", saved });
     } catch (error) {
         console.log(error);
         res.status(500).json({error:'server error'})
